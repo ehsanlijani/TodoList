03fd6225b43db935bf56cd95b047d4c1793cbc9e
Ehsan Lijani <ehsanlijani@yahoo.com>
2025-03-18 23:10:52 +0330

    Add project files.

---
+++ TodoList.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿namespace TodoList.Domain.Entities.Common;
+
+#nullable enable
+
+public class BaseEntity<T>
+{
+    public required T Id { get; set; }
+
+    public DateTime? DeletedTime { get; set; }
+
+    public bool IsDeleted
+    {
+        get
+        {
+            if (DeletedTime is not null || DeletedTime != DateTime.MinValue)
+                return true;
+
+            else
+                return false;
+        }
+    }
+
+    public required DateTime CreatedAt { get; set; }
+
+    public DateTime? ModifiedAt { get; set; }
+}
---
+++ TodoList.Domain/Entities/Task.cs
@@ -0,0 +1,16 @@
+﻿using TodoList.Domain.Entities.Common;
+
+#nullable disable
+
+namespace TodoList.Domain.Entities;
+
+public class Task : BaseEntity<Guid>
+{
+    public required string Description { get; set; }
+
+    public required string Title { get; set; }
+
+    public required bool IsCompleted { get; set; }
+
+    public required DateTime DueDate { get; set; }
+}
