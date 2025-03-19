9d7c506366eb2950cbf250d5ce9896574e171dff
Ehsan Lijani <ehsanlijani@yahoo.com>
2025-03-19 11:18:23 +0330

    + Add Result pattern to project

---
+++ TodoList.Application/Wrappers/Error.cs
@@ -0,0 +1,33 @@
+﻿namespace TodoList.Application.Wrappers;
+
+public record Error
+{
+    public static readonly Error None = new(string.Empty, string.Empty, ErrorType.None);
+
+    public static implicit operator Result(Error error)
+        => Result.Failure(error);
+
+    private Error(string code, string message, ErrorType type)
+    {
+        Code = code;
+        Message = message;
+        Type = type;
+    }
+
+    public string Code { get; set; }
+    public string Message { get; set; }
+    public ErrorType Type { get; set; }
+
+    public static Error NotFound(string code, string message)
+        => new(code, message, ErrorType.NotFound);
+
+    public static Error Exception(string code, string message)
+        => new(code, message, ErrorType.Failure);
+
+    public static Error Failure(string code, string message)
+        => new(code, message, ErrorType.None);
+
+    public static Error Validation(string code, string message)
+        => new(code, message, ErrorType.Validation);
+}
+
